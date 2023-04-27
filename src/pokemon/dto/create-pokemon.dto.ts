1e89ac7d7d7848c6c1a7f9821803a41f865aa5e0
osmelpm <opmilan95@gmail.com>
2023-04-27 17:08:25 -0400

    Add create method and validation errors

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsString, IsInt, MinLength, IsPositive, Min } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common/exceptions';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+    try {
+      return await this.pokemonModel.create(createPokemonDto);
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(
+          `Duplicate key ${JSON.stringify(error.keyValue)}`,
+        );
+      }
+
+      console.log(error);
+      throw new InternalServerErrorException(
+        "Can't create pokemon - Check the logs",
+      );
+    }
   }
 
   findAll() {
